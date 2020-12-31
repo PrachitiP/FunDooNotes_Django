70530421e39b3f9adb40c041cb0667710041d53a
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2020-12-31 15:40:41 +0530

    URLs for app

--- FundooNotesProject/FundooNotesApp/urls.py
+++ FundooNotesProject/FundooNotesApp/urls.py
@@ -1,2 +1,13 @@
-from django.urls import path
-from .views import registerForm
+from django.conf.urls import url
+from django.urls import path,include
+from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
+from .import views
+from .views import registerform, EmailVerification
+from rest_framework import permissions
+
+
+urlpatterns=[
+     path('reg/',registerform.as_view()),
+     path('email-verify/',EmailVerification.as_view()),
+]
