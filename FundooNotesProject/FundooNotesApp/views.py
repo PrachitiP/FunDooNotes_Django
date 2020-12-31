f2ba81dfbaab9eb3eba68f26dd358330ec2242e0
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2020-12-31 15:36:55 +0530

    Code for registartion classs

--- FundooNotesProject/FundooNotesApp/views.py
+++ FundooNotesProject/FundooNotesApp/views.py
@@ -1,24 +1,60 @@
-from django.shortcuts import render
-from rest_framework.generics import GenericAPIView
+from email.message import EmailMessage
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.urls import reverse
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, status, generics
 from rest_framework.response import Response
-from .serializer import RegisterationFormSerializer
+from rest_framework_jwt.serializers import jwt_payload_handler
+from rest_framework_simplejwt.tokens import RefreshToken
+from . import serializer
+from .models import UserDetails
+from .serializer import UserDeatilsSerializer, EmailVerificationSerializer
+from django.core.mail import send_mail
+from django.conf import settings
+from .token import token_activation
+from .utils import Util
+import views
+import logging
+import jwt
 
-class registerForm(GenericAPIView):
-    serializer_class = RegisterationFormSerializer
+class registerform(generics.GenericAPIView):
+    """
+        A Registration class for registration of users with information
+
+    """
+    queryset = UserDetails.objects.all()
+    serializer_class = UserDeatilsSerializer
+
 
     def post(self, request):
+        """
+            Defined post method which register users with valid email and oher inputs.
+            It checks the authenticity of the email by sending a activation link for user to the respective email
+             
+        """
         try:
-            userName = request.data['username']
-            email = request.data['email']
-            password = request.data['password']
-            confirm_password = request.data['confirm_password']
+            user = request.data
+            serializer = self.serializer_class(data=user)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            user_data = serializer.data
+            user = User.objects.get(email=user_data['email'])
+            user.is_active=False
+            payload = jwt_payload_handler(user)
+            token = jwt.encode(payload, settings.SECRET_KEY).decode('UTF-8')
+            current_site = get_current_site(request).domain
+            relativeLink = '/email-verify/'
+            absurl = 'http://' + current_site + relativeLink + "?token=" + str(token)
+            email_body = 'Hi ' + user.username +  ' Use the link below to verify your email \n' + absurl
+            data = {'email_body': email_body, 'to_email': user.email,  'email_subject': 'Verify your email'}
+            Util.send_email(data)
+            logging.debug('validated data: {}'.format(serializer.data))
+            return Response(user_data, status=status.HTTP_201_CREATED)
 
-            if userName == "" or email == "" or password == "":
-                return Response("You can not put empty fields", status=status.HTTP_406_NOT_ACCEPTABLE)
-            if password == confirm_password:
-                try:
-                    validate_email(email)
-                    user = User.objects.create_user(
-                        username=userName, email=email, password=password)
-                    user.is_active = False
-                    user.save()
+        except Exception:
+           return Response(serializer.errors)
