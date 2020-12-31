6b862c33a4f2391f054a569e341b1868596d8feb
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2020-12-31 15:53:28 +0530

    Serializer class for email verification

--- FundooNotesProject/FundooNotesApp/serializer.py
+++ FundooNotesProject/FundooNotesApp/serializer.py
@@ -6,3 +6,9 @@ class RegisterationFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDetails
         fields = ['username','email','password','confirm_password']
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+    class Meta:
+        model = User
+        fields = ['token']
