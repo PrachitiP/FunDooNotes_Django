76e1475f08a95b371780373ca40df00168a350a9
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2021-01-02 16:20:04 +0530

    Serializer class for Login the user

--- FundooNotesProject/FundooNotesApp/serializer.py
+++ FundooNotesProject/FundooNotesApp/serializer.py
@@ -12,3 +12,25 @@ class EmailVerificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['token']
+       
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    username = serializers.CharField(max_length=255, min_length=3, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'username']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+        user = auth.authenticate(email=email, password=password)
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        if not user.is_active:
+            raise AuthenticationFailed('Account is not activated yet')
+
+        return {
+            'username': user.username, 'email': user.email,
+        }
