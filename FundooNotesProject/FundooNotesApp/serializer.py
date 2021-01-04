05dd5be2809c5d36c4105703a86a64ed0479ec34
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2021-01-04 12:28:29 +0530

    Serializer class for forget password

--- FundooNotesProject/FundooNotesApp/serializer.py
+++ FundooNotesProject/FundooNotesApp/serializer.py
@@ -34,3 +34,12 @@ class LoginSerializer(serializers.ModelSerializer):
         return {
             'username': user.username, 'email': user.email,
         }
+    
+    class ForgetPasswordtSerializer(serializers.Serializer):
+    email = serializers.EmailField(min_length=2)
+
+    redirect_url = serializers.CharField(max_length=500, required=False)
+
+    class Meta:
+        fields = ['email']
+
