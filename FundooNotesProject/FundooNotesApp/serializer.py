76e50407bfa0c4cc35f18f685de93ce28867a5b1
PrachitiP <68328603+PrachitiP@users.noreply.github.com>
2021-01-05 09:45:32 +0530

    Serializer class for reset password

--- FundooNotesProject/FundooNotesApp/serializer.py
+++ FundooNotesProject/FundooNotesApp/serializer.py
@@ -34,3 +34,39 @@ class LoginSerializer(serializers.ModelSerializer):
         return {
             'username': user.username, 'email': user.email,
         }
+class ResetPasswordSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        try:
+            user = User.objects.get(email=email)
+            if not user.is_active:
+                raise serializers.ValidationError("This email id is not active")
+        except User.DoesNotExist:
+            raise serializers.ValidationError("This email is not registerd")
+
+        return attrs
+
+
+class NewPasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, min_length=6)
+    NewPassword=serializers.CharField(max_length=68, min_length=6)
+
+
+    class Meta:
+        model = User
+        fields = ['password', 'NewPassword']
+
+    def validate(self, attrs):
+        password = attrs.get('password', '')
+        NewPassword =attrs.get('NewPassword', '')
+
+        if password != NewPassword:
+            raise serializers.ValidationError("Password not matched!!")
+
+        return attrs
